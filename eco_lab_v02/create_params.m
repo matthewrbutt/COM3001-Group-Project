32bce933e19750b36c8f4ad152268e0defcbbe9d
Matthew Butt <mrbutt1@sheffield.ac.uk>
2020-03-17 14:42:43 +0000

    Changed healthy people speed

--- eco_lab_v02/create_params.m
+++ eco_lab_v02/create_params.m
@@ -8,7 +8,7 @@ function create_params
 
 global PARAM
 
-    PARAM.R_SPD=50;         %speed of movement - units per itn (rabbit)
+    PARAM.R_SPD=10;         %speed of movement - units per itn (rabbit)
     PARAM.F_SPD=0;         %speed of movement - units per itn (fox)
     PARAM.R_MAXAGE=10000;      %maximum age allowed 
     PARAM.F_MAXAGE=336;
