b3066dc65d3d823dfd9fc1106adc1faa76636586
Matthew Butt <mrbutt1@sheffield.ac.uk>
2020-03-18 13:21:54 +0000

    added modified comments (for clarity)

--- eco_lab_v02/agnt_solve.m
+++ eco_lab_v02/agnt_solve.m
@@ -19,13 +19,12 @@ prev_n=n;   %remember current agent number at the start of this iteration
 for cn=1:n
 	curr=agent{cn};
     if isa(curr,'rabbit')|isa(curr,'fox')
-        %[curr,eaten]=eat(curr,cn);               %eating rules (rabbits eat food, foxes eat rabbits
-        curr=migrate(curr,cn);              %if no food was eaten, then migrate in search of some
-        [curr,klld]=die(curr,cn);                %death rule (from starvation or old age)
+        curr=migrate(curr,cn);                  %determine direction of movement
+        [curr,klld]=die(curr,cn);               %check for infection
         if klld==1
             new=[];
-            [curr,new]=breed(curr,cn);			%breeding rule
-            if ~isempty(new)					%if current agent has bred during this iteration
+            [curr,new]=breed(curr,cn);			%if infected, "die" and create new infected agent
+            if ~isempty(new)					%if current agent has "died" during this iteration
                  n_new=n_new+1;                 %increase new agent number
                  agent{n+n_new}=new;			%add new to end of agent list
              end
