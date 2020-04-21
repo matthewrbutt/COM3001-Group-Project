ede4eea9a5d26d8ba6e0c2471cd05dbc9de4451b
Peter Bull <pmrbull1@sheffield.ac.uk>
2020-04-21 16:59:57 +0100

    Changed speed

--- eco_lab_v02/create_params.m
+++ eco_lab_v02/create_params.m
@@ -8,8 +8,8 @@ function create_params
 
 global PARAM
 
-    PARAM.R_SPD=10;         %speed of movement - units per itn (healthy human)
-    PARAM.F_SPD=10;          %speed of movement - units per itn (infected human)
+    PARAM.R_SPD=2;         %speed of movement - units per itn (healthy human)
+    PARAM.F_SPD=2;          %speed of movement - units per itn (infected human)
     PARAM.R_MAXAGE=10000;   %maximum agent age allowed 
     PARAM.F_MAXAGE=normrnd(120,48);    %duration of symptoms (mean = 5 days, s.d. = 2 days)
     PARAM.C_MAXAGE=normrnd(48,24);      %incubation period (mean = 2 days, s.d. = 1 day)
